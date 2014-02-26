1b46d119294fb3237b44c93c037fe819fe052f2a
Kevin <kevinchanthasiriphan@gmail.com>
2014-02-25 20:19:45 -0800

    Initial commit

---
+++ app/controllers/rockpaperscissors_controller.rb
@@ -0,0 +1,2 @@
+class RockpaperscissorsController < ApplicationController
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RockPaperScissors::Application.config.secret_key_base = '260d8fc5515931978f87a3c941ce9fa90e8746f015bf0b9cfb9b44bf0ed1793b88d76c1003e127f10a38b28d37753a726d5556ce9d83ad4d29292444b2804e96'
